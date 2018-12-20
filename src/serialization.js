392b73b4dd79ec8141fee0ec4a78f3fac17d6493
Richard Feldman <richard.t.feldman@gmail.com>
2018-12-20 08:49:49 -0300

    Got nested arrays working

--- src/serialization.js
+++ src/serialization.js
@@ -7,32 +7,96 @@ function decode(bytes) {
 
   switch (variantType) {
     case 0x1: // Topology
-      return decodeTopology(8, dv);
+      var ptable = dv.getUint16(6, true);
+
+      return decodeTopology(dv, 8, ptable);
   }
 
   decodingError(bytes, "I couldn't decode this unknown variant type: " + variantType);
 }
 
-function decodeTopology(address, dv) {
+function decodeTopology(dv, addr, ptable) {
+  // TODO check if ptable is missing any required entries
+  
   return {
     type: "Topology",
-    objects: dv.getFloat64(address, true), // objects is a float64
-    arcs: dv.getFloat64(address + 8, true), // arcs is a float64
-    transform: dv.getFloat64(address + 16, true) // transform is a float64
+    objects: decodeObjects(dv, addr),
+    arcs: decodeArcs(dv, addr + 8),
+    transform: decodeTransform(dv, addr + 16)
   };
 }
 
+function decodeObjects(dv, addr) {
+  var objAddr = dv.getUint32(addr, true);
+  var ptable = dv.getUint32(addr + 4, true);
+
+  // TODO
+}
+
+function decodeTransform(dv, addr) {
+  var objAddr = dv.getUint32(addr, true);
+  var ptable = dv.getUint32(addr + 4, true);
+
+  // TODO
+}
+
+function decodeArcs(dv, addr) {
+  var arcsAddr = dv.getUint32(addr, true);
+  var arcsLength = dv.getUint32(addr + 4, true);
+  
+  if (arcsLength === 0) {
+    if (arcsAddr === 0) {
+      // empty collection; nothing to look up!
+      return [];
+    }
+
+    throw "TODO handle superlong pointers.";
+  } else {
+    // Since length > 0 && address > 0, this is a normal pointer - look it up!
+    var arcs = new Array(arcsLength);
+
+    for (var i=0; i < arcsLength; i++) {
+      arcs[i] = decodeInt32Array(dv, (arcsAddr + i) * 8);
+    }
+
+    return arcs;
+  }
+}
+
+function decodeInt32Array(dv, addr) {
+  var intsAddr = dv.getUint32(addr, true);
+  var intsLength = dv.getUint32(addr + 4, true);
+  
+  if (intsLength === 0) {
+    if (intsAddr === 0) {
+      // empty collection; nothing to look up!
+      return [];
+    }
+
+    throw "TODO handle superlong pointers.";
+  } else {
+    // Since length > 0 && address > 0, this is a normal pointer - look it up!
+    var ints = new Array(intsLength);
+
+    for (var i=0; i < intsLength; i++) {
+      ints[i] = dv.getInt32((intsAddr + i) * 8, true);
+    }
+
+    return ints;
+  }
+}
+
 function encode(data) {
-  var bytes = new ArrayBuffer(1024 * 1024); // TODO dynamic allocation
+  var bytes = new ArrayBuffer(20000); // TODO dynamic allocation
   var dv = new DataView(bytes);
   
   switch (data.type) {
     case "Topology":
       dv.setUint32(0, 1, true); // This is Release #1
       dv.setUint16(4, 0x1, true); // 0x1 is the "Topology" variant
-      dv.setUint16(6, 3, true); // Word length of the "Topoloy" variant is 3 (an inlined record)
+      dv.setUint16(6, 0x111, true); // The "Topoloy" variant holds a record; this is its ptable
       
-      encodeTopology(dv, 8, data.objects, data.arcs, data.transform);
+      encodeTopology(dv, 8, 2, data.objects, data.arcs, data.transform);
 
       return dv.buffer;
   }
@@ -40,10 +104,60 @@ function encode(data) {
   encodingError(data, "I couldn't encode this unknown variant type: " + data.type);
 }
 
-function encodeTopology(dv, address, objects, arcs, transform) {
-  dv.setFloat64(address, objects, true); // objects is a float64
-  dv.setFloat64(address + 8, arcs, true); // arcs is a float64
-  dv.setFloat64(address + 16, transform, true); // transform is a float64
+function encodeTopology(dv, addr, nextAddr, objects, arcs, transform) {
+  nextAddr += 3; // Advance past this Topology record's 3 fields
+ 
+  nextAddr = encodeObjects(  dv, addr,      nextAddr, objects); 
+  nextAddr = encodeArcs(     dv, addr + 8,  nextAddr, arcs);
+  nextAddr = encodeTransform(dv, addr + 16, nextAddr, transform);
+
+  return nextAddr;
+}
+
+function encodeObjects(dv, addr, nextAddr, objects) {
+  dv.setUint32(addr, nextAddr, true);
+  dv.setUint32(addr + 4, 0x111, true); // presence table for Objects record
+
+  return nextAddr + 3; // Advance past Objects record's 3 fields
+}
+
+function encodeTransform(dv, addr, nextAddr, objects) {
+  dv.setUint32(addr, nextAddr, true);
+  dv.setUint32(addr + 4, 0x11, true); // presence table for Transform record
+
+  return nextAddr + 1; // Advance past my 1 pointer.
+}
+
+function encodeArcs(dv, addr, nextAddr, arcs) {
+  // Leave it at all 0s if it's an empty collection.
+  if (arcs.length !== 0) {
+    dv.setUint32(addr, nextAddr, true);
+    dv.setUint32(addr + 4, arcs.length, true);
+
+    var nextArcAddr = nextAddr + arcs.length; // Advance past the array elements
+
+    for (var i=0; i < arcs.length; i++) {
+      nextArcAddr = encodeInt32Array(dv, (nextAddr++ * 8), nextArcAddr, arcs[i]);
+    }
+
+    nextAddr = nextArcAddr;
+  }
+
+  return nextAddr;
+}
+
+function encodeInt32Array(dv, addr, nextAddr, ints) {
+  // Leave it at all 0s if it's an empty collection.
+  if (ints.length !== 0) {
+    dv.setUint32(addr, nextAddr, true);
+    dv.setUint32(addr + 4, ints.length, true);
+
+    for (var i=0; i < ints.length; i++) {
+      dv.setUint32((nextAddr++ * 8), ints[i], true);
+    }
+  }
+
+  return nextAddr;
 }
 
 function encodingError(data, msg) {
@@ -82,6 +196,6 @@ function verify(data) {
 verify({
   type: "Topology",
   objects: 1.2,
-  arcs: 3.4,
+  arcs: [[1], [2, 3], [44, 55, 66], [], [77]],
   transform: 5.6
 });
