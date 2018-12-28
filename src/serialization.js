501dcb8913d549301d25e0824a0376fc0cd24a14
Richard Feldman <richard.t.feldman@gmail.com>
2018-12-27 23:15:32 -0300

    Failed attempt

--- src/serialization.js
+++ src/serialization.js
@@ -1,4 +1,7 @@
 
+// TODO refactor to use es6.js
+
+
 function logBytes(bytes) {
   let str = "";
 
@@ -26,6 +29,30 @@ function logBytes(bytes) {
 
 }
 
+/** Writes the given destAddrWord and lengthOrPtable into the given 
+ * DataView at the given address.
+ */
+function writePtr(dv, byteAddr, destWordAddr, lengthOrPtable) {
+  dv.setUint32(byteAddr, destWordAddr, true);
+  dv.setUint32(byteAddr + 4, lengthOrPtable, true); 
+}
+
+// function writeRec(dv, byteAddr, destWordAddr, fieldWriters) {
+//   writePtr(dv, byteAddr, destWordAddr);
+
+//   var destByteAddr = destWordAddr * 8;
+
+//   destWordAddr += fieldWriters.length; // Advance past this record's fields
+ 
+//   for (var i=0; i < fieldWriters.length; i++) {
+//     var writeField = fieldWriters[i];
+
+//     destWordAddr = writeField(dv, destByteAddr + (i * 8), destWordAddr);
+//   }
+
+//   return destWordAddr;
+// }
+
 
 function decode(bytes) {
   var dv = new DataView(bytes);
@@ -48,20 +75,20 @@ function decodeTopology(dv, addr, ptable) {
   return {
     type: "Topology",
     objects: decodeObjects(dv, addr),
-    arcs: decodeArcs(dv, addr + 8),
-    transform: decodeTransform(dv, addr + 16)
+    // arcs: decodeArcs(dv, addr + 8),
+    // transform: decodeTransform(dv, addr + 16)
   };
 }
 
 function decodeObjects(dv, addr) {
-  var objAddr = dv.getUint32(addr, true) * 8;
+  var destAddr = dv.getUint32(addr, true) * 8;
   var ptable = dv.getUint32(addr + 4, true);
 
   // TODO check if ptable is missing any required entries
   
   return {
-    counties: decodeGeometryCollection(dv, objAddr),
-    states: decodeGeometryCollection(dv, objAddr + 8),
+    counties: decodeGeometryCollection(dv, destAddr),
+    states: decodeGeometryCollection(dv, destAddr + 8),
     // land: decodeGeometry(dv, (objAdder + 2) * 8)
   };
 }
@@ -187,30 +214,25 @@ function encode(data) {
   encodingError(data, "I couldn't encode this unknown variant type: " + data.type);
 }
 
-function encodeGeometryCollection(dv, addr, nextAddr, geometries, bbox) {
-  var ptable = 
-    bbox == null
-      ? 0x01
-      : 0x11;
+function encodeGeometryCollection(dv, byteAddr, destWordAddr, geometries, bbox) {
+  var ptable = bbox == null ? 0x01 : 0x11;
+  writePtr(dv, byteAddr, destWordAddr, ptable);
 
-  dv.setUint32(addr, nextAddr, true);
-  dv.setUint32(addr + 4, ptable, true); // presence table for GeometryCollection record
+  var destByteAddr = destWordAddr * 8;
 
-  var fieldAddr = nextAddr * 8;
-
-  nextAddr += 1; // Advance past this GeometryCollection record's 1 required field
+  destWordAddr += 1; // Advance past this GeometryCollection record's 1 required field
 
   if (bbox != null) {
-    nextAddr += 1; // Advance past this GeometryCollection record's 1 optional field which happened to be present
+    destWordAddr += 1; // Advance past this GeometryCollection record's 1 optional field which happened to be present
   }
  
-  nextAddr = encodeGeometries(dv, fieldAddr, nextAddr, geometries); 
+  destWordAddr = encodeGeometries(dv, destByteAddr, destWordAddr, geometries); 
 
   if (bbox != null) {
-    nextAddr = encodeBBox(dv, fieldAddr + 8, nextAddr, bbox); 
+    destWordAddr = encodeBBox(dv, destByteAddr + 8, destWordAddr, bbox); 
   }
 
-  return nextAddr;
+  return destWordAddr;
 }
 
 
@@ -219,14 +241,16 @@ function encodeGeometries(dv, addr, nextAddr, geometries) {
 }
 
 
-function encodeTopology(dv, addr, nextAddr, objects, arcs, transform) {
-  nextAddr += 3; // Advance past this Topology record's 3 fields
- 
-  nextAddr = encodeObjects(  dv, addr,      nextAddr, objects); 
-  nextAddr = encodeArcs(     dv, addr + 8,  nextAddr, arcs);
-  nextAddr = encodeTransform(dv, addr + 16, nextAddr, transform);
+function encodeTopology(dv, destWordAddr, objects, arcs, transform) {
+  var destByteAddr = destWordAddr * 8;
 
-  return nextAddr;
+  destWordAddr += 3; // Advance past this Topology record's 3 fields
+ 
+  destWordAddr = encodeObjects(  dv, destByteAddr,      destWordAddr, objects); 
+  destWordAddr = encodeArcs(     dv, destByteAddr + 8,  destWordAddr, arcs);
+  destWordAddr = encodeTransform(dv, destByteAddr + 16, destWordAddr, transform);
+
+  return destWordAddr;
 }
 
 function encodeObjects(dv, addr, nextAddr, objects) {
@@ -257,19 +281,16 @@ function encodeTransform(dv, addr, nextAddr, transform) {
   return nextAddr;
 }
 
-function encodeBBox(dv, addr, nextAddr, bbox) {
-  dv.setUint32(addr, nextAddr, true);
-  dv.setUint32(addr + 4, 0x1111, true); // presence table for BBox record
+function encodeBBox(dv, byteAddr, destWordAddr, bbox) {
+  writePtr(dv, byteAddr, destWordAddr, 0x1111);
 
-  dv.setFloat32(nextAddr++ * 8, bbox[0], true);
-  dv.setFloat32(nextAddr++ * 8, bbox[1], true);
-  dv.setFloat32(nextAddr++ * 8, bbox[2], true);
-  dv.setFloat32(nextAddr++ * 8, bbox[3], true);
+  dv.setFloat32(destWordAddr++ * 8, bbox[0], true);
+  dv.setFloat32(destWordAddr++ * 8, bbox[1], true);
+  dv.setFloat32(destWordAddr++ * 8, bbox[2], true);
+  dv.setFloat32(destWordAddr++ * 8, bbox[3], true);
  
-  return nextAddr;
+  return destWordAddr;
 }
-
-
 
 function encodeArcs(dv, addr, nextAddr, arcs) {
   // Leave it at all 0s if it's an empty collection.
